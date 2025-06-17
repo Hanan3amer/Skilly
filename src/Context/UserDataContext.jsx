d6c8a7c2d4bb006979b23067342f0d52bc932bae
Hanan3amer <hananamer312003@gmail.com>
2025-06-18 00:08:08 +0300

    handel refresh in rewards

--- src/Context/UserDataContext.jsx
+++ src/Context/UserDataContext.jsx
@@ -1,42 +1,50 @@
 import { createContext, useState, useEffect } from "react";
 import axios from "axios";
 import PropTypes from "prop-types";
+
 export const UserDataContext = createContext();
 
 export function UserDataProvider({ children }) {
   const [user, setUser] = useState(null);
-  const token = localStorage.getItem("userToken");
+  const [token, setToken] = useState(localStorage.getItem("userToken"));
 
-  function getUser() {
-    axios
-      .get(
-        "https://skilly.runasp.net/api/UserProfile/UserProfile/GetUserProfileByuserId",
-        {
-          headers: {
-            Authorization: `Bearer ${token}`,
-          },
-        }
-      )
-      .then((response) => {
-        setUser(response.data.user);
-      })
-      .catch((error) => console.log(error));
-  }
+  useEffect(() => {
+    const interval = setInterval(() => {
+      const currentToken = localStorage.getItem("userToken");
+      if (currentToken !== token) {
+        setToken(currentToken);
+      }
+    }, 1000);
+
+    return () => clearInterval(interval);
+  }, [token]);
 
   useEffect(() => {
     if (token) {
-      getUser();
+      axios
+        .get(
+          "https://skilly.runasp.net/api/UserProfile/userProfile/GetUserProfileByuserId",
+          {
+            headers: {
+              Authorization: `Bearer ${token}`,
+            },
+          }
+        )
+        .then((response) => {
+          setUser(response.data.user);
+          localStorage.setItem("userD", JSON.stringify(response.data.user));
+        })
+        .catch((error) => console.log(error));
     }
   }, [token]);
-  const updateUser = (newUserData) => {
-    setUser(newUserData);
-  };
+
   return (
-    <UserDataContext.Provider value={{ user, updateUser }}>
+    <UserDataContext.Provider value={{ user }}>
       {children}
     </UserDataContext.Provider>
   );
 }
+
 UserDataProvider.propTypes = {
   children: PropTypes.node,
 };
